5deb129ea84e39440fd8dfde4432de7f53756b35
David Freeman <davfree0@gmail.com>
2022-12-13 22:59:13 +0200

    Finishig with rest API for user and goals.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,9 @@
 import {useState, useEffect} from 'react';
 import {FaSignInAlt} from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {login, reset} from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -8,6 +12,24 @@ function Login() {
   });
 
   const {name, password} = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -18,6 +40,17 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      name, 
+      password,
+    }
+  
+    dispatch(login(userData));
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,9 @@
 import {useState, useEffect} from 'react';
 import {FaUser} from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {register, reset} from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -10,6 +14,24 @@ function Register() {
 
   const {name, password, password2} = formData;
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth);
+
+  useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -19,6 +41,20 @@ function Register() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      const userData = {
+        name, password
+      };
+
+      dispatch(register(userData));
+    }
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
